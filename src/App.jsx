11cbc5b07abe1273aa77422ca6e7260706c8021e
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-15 13:34:09 +0300

    initial styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 
+import { GeneralInfo } from './components';
+
 import './App.css'
 
 function App() {
   return (
-    <>
-      <h1>CV Application</h1>
-    </>
+    <main className="container">
+      <h1>CV Builder</h1>
+
+      <GeneralInfo />
+    </main>
   );
 }
 
---
+++ src/components/GeneralInfo/GeneralInfo.jsx
@@ -0,0 +1,9 @@
+function GeneralInfo() {
+  return (
+    <section>
+      General Info
+    </section>
+  );
+}
+
+export default GeneralInfo;
