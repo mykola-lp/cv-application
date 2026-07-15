5c306ca8911ccb7786ca547f3890519b66fd16fb
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-15 13:50:51 +0300

    general info component with styles

--- src/components/GeneralInfo/GeneralInfo.jsx
+++ src/components/GeneralInfo/GeneralInfo.jsx
@@ -1,7 +1,26 @@
+import "./GeneralInfo.css";
+
 function GeneralInfo() {
   return (
-    <section>
-      General Info
+    <section className="general-info">
+      <h2>General Information</h2>
+
+      <label>
+        Name
+        <input type="text" />
+      </label>
+
+      <label>
+        Email
+        <input type="email" />
+      </label>
+
+      <label>
+        Phone
+        <input type="tel" />
+      </label>
+
+      <button>Save</button>
     </section>
   );
 }
